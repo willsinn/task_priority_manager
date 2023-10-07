7171e0eb61bb137520a91d690561f1448c3932aa
William Sinn <30275717+willsinn@users.noreply.github.com>
2023-10-07 00:50:19 -0400

    Add Copy New Task to Priority Manager, Add Update Priority Manager Ids on Project Sheet Name Change, Improve Id Update Function for Active Sheet
    ready to sync values by task ID

--- enableCrossSheetTaskSync.js
+++ enableCrossSheetTaskSync.js
@@ -1,4 +1,6 @@
+const prioritySheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('priority_manager');
 const priorityArray = ["Critical", "High", "Medium", "Low"];
+const priorityColA = prioritySheet.getRange('A:A').getValues();
 
 
 const activeSheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
@@ -8,56 +10,68 @@ const cellTaskCount = activeSheet.getRange('A2');
 
 const activeCell = activeSheet.getActiveCell();
 const rowIdx = activeCell.getRowIndex();
-    const colA = activeSheet.getRange('A:A').getValues();
+const colA = activeSheet.getRange('A:A').getValues();
 
 const handleUpdateIDsOnSheetNameChange = () => colA.map((tId, idx) => { //map through all existing IDs and update them to match the new sheet name
-      const task = { idvalue: "" };
-
       if (idx > 1 && tId[0]) { //skip first two rows
-          let tIdSplit = tId[0].split("");
-
-          for (let i = 0; i < 5; i++) { // account for a 6 digit ID number
-              const lastLetter = tIdSplit.pop();
-              if (lastLetter == "_" || !parseInt(lastLetter)) { // if last letter is NaN
-                  break;
-              } else if (!task.idvalue) {
-                  task.idvalue = `${lastLetter}`;
-              } else {
-                  const str = `${lastLetter}${task.idvalue}`
-                  task.idvalue = str;
-                }
-            }
-
-            const newId = `${sheetName}` + '_' + `${task.idvalue}`;
+          const getIdParts = tId[0].split("__", 2);
+            const newId = `${sheetName}` + '__' + `${getIdParts[1]}`;
             const targetCell = activeSheet.getRange(`A${parseInt(idx+1)}`)
             targetCell.setValue(newId);
-        }
-      })
+      }  
+    })
+
 
 function handleUpdateCellA1() {
       const getSheetNameCell = activeSheet.getRange('A1');
       getSheetNameCell.setValue(sheetName);
-      return;
 }
+
+const handleUpdatePriorityIds = () => priorityColA.map((pId, idx) => {
+    if (idx > 1 && pId[0]) {
+    const getIdParts = pId[0].split("__", 2);
+        if (getIdParts[0] == cellSheetNameValue) {
+            const newId = `${sheetName}` + '__' + `${getIdParts[1]}`;
+            const targetCell = prioritySheet.getRange(`A${parseInt(idx+1)}`)
+            targetCell.setValue(newId);
+        }
+    }
+})
 
 function handleSheetNameChange() {
       if (sheetName !== cellSheetNameValue) {
-        handleUpdateIDsOnSheetNameChange()
+        handleUpdateIDsOnSheetNameChange();
+        handleUpdatePriorityIds();
         handleUpdateCellA1();
       }
     }
+
+function handleCopyNewTaskToPriorityManager(tId) {
+    if (tId) {
+      const numCols = activeSheet.getMaxColumns();
+      const newTaskRow = activeSheet.getRange(rowIdx, 1, 1, numCols);
+      const newTaskValues = newTaskRow.getValues()
+      const getPrioSheetRowIdx = prioritySheet.getLastRow();
+      prioritySheet.insertRowAfter(getPrioSheetRowIdx); //insert new row
+      prioritySheet.getRange(getPrioSheetRowIdx + 1, 1, 1, newTaskValues[0].length).setValues(newTaskValues)
+      handleSheetNameChange();
+    }
+}
 
 function handleCreateUniqueTaskID() {
     const taskIdCell = activeSheet.getRange(`A${rowIdx}`);
 
     if (!taskIdCell.getValue()) {
-        const taskId = `${cellSheetNameValue}` + "_" + `${cellTaskCount.getValue()}`;
-
+        const taskId = `${sheetName}` + "__" + `${cellTaskCount.getValue()}`;
         activeSheet.getRange('A2').setValue(cellTaskCount.getValue() +1); //update task counter
-
         taskIdCell.setValue(`${taskId}`); //add id to first column
-    } 
+                handleUpdatePriorityIds();
+        handleCopyNewTaskToPriorityManager(taskId);
+    } else {
+        handleSheetNameChange()
+    }
   }
+
 
 function onEdit(e) { 
     const activeCellValue = activeCell.getValue();
